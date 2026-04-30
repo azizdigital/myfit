d75207d505d322c064d14c1b4794f1f5d165cdb2
Aziz Mohamad_AI <aziz.mohamad_ai@icloud.com>
2026-04-30 13:15:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // ═══════════════════════════════════════════════
 
 var DEFAULT_GH_REPO   = 'azizdigital/myfit-data.git';  // your PRIVATE data repo
-var DEFAULT_GH_TOKEN  = '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';     // ghp_xxxxxxxxxxxxxxxxxxxx
+var DEFAULT_GH_TOKEN  = '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';     // ghp_xxxxxxxxxxxxxxxxxxxx
 var DEFAULT_GH_BRANCH = 'main';
