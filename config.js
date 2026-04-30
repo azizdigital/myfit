e7ef773f514374f8329f9b3bf28848e824d82c92
Aziz Mohamad_AI <aziz.mohamad_ai@icloud.com>
2026-04-30 13:20:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // Edit this file to set your GitHub repo & token
 // ═══════════════════════════════════════════════
 
-var DEFAULT_GH_REPO   = 'azizdigital/myfit-data.git';  // your PRIVATE data repo
+var DEFAULT_GH_REPO   = 'azizdigital/myfit-data';  // your PRIVATE data repo
 var DEFAULT_GH_TOKEN  = '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';     // ghp_xxxxxxxxxxxxxxxxxxxx
 var DEFAULT_GH_BRANCH = 'main';
